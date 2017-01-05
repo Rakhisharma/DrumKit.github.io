e88fed2fb3e6b7a02fa29fa6c980add8b1d359cf
Rakhi Sharma <rakhish1994@gmail.com>
2017-01-05 18:54:21 +0530

    update

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ function playSound(e) {
 	//to stop funtion running all together
 	if (!audio) return;
 	//setting CurrentTime so that it will rewind to the start
-	audio.CurrentTime = 0;
+	audio.currentTime = 0;
 	audio.play();
 	key.classList.add('playing');
 }
-
 // to remove the transition after 0.07 second. see index.css for more!
 function removeTransition(e) {
 	if (e.propertyName !== 'transform') return;
 	this.classList.remove('playing');
-}
- 
-const keys = document.querySelectorAll('.key');
+} 
+
+const keys = Array.from(document.querySelectorAll('.key'));
+//keys.addEventListener('transitionend', removeTransition);
+//if(!Array.prototype.forEach) {
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
- 
+
 //adding event listener. It will listen to keydown!
 window.addEventListener('keydown', playSound);
